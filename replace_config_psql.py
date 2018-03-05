d02b2ad61216e8841f4a4b1f6736c871df8acede
Felipe Hernández <cephei.1313@gmail.com>
2018-03-05 15:17:03 -0300

    updated script

--- replace_config_psql.py
+++ replace_config_psql.py
@@ -1,35 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import sys
 import os
 
-if len(sys.argv) < 2:
-    pass
-else:
-    path = '/etc/postgresql/' + sys.argv[1] + '/main/pg_hba.conf'
-    additional_line = 'local   all             all                                     md5\n'
 
-    new_lines = []
-    header_found = False
-    pass_one = False
+def main():
+    if len(sys.argv) < 2:
+        pass
+    else:
+        path = '/etc/postgresql/{0}/main/pg_hba.conf'.format(sys.argv[1])
+        additional_line = 'local   all             all                                     md5\n'
 
-    with open(path) as FILE:
-        for line in FILE:
-            if header_found and line == additional_line:
-               
-            if pass_one:
-                pass_one = False
-                continue
+        new_lines = []
+        header_found = False
 
-            if line == '''# "local" is for Unix domain socket connections only\n''':
-                header_found = True
-                os.system("echo done")
+        with open(path) as FILE:
+            for line in FILE:
+                if header_found and line != additional_line:
+                    # if additional line was added previously, ignore it
+                    new_lines.append(additional_line)
+                    os.system("echo done")
+
+                if line == '''# "local" is for Unix domain socket connections only\n''':
+                    header_found = True
+
                 new_lines.append(line)
-                new_lines.append(additional_line)
-                pass_one = True
-            else:
-                new_lines.append(line)
- 
-    with open(path, 'w') as CONFIG_FILE:
-        for line in new_lines:
-            CONFIG_FILE.write(line)
-            CONFIG_FILE.close()
 
+        with open(path, 'w') as CONFIG_FILE:
+            for line in new_lines:
+                CONFIG_FILE.write(line)
+                CONFIG_FILE.close()
+
+
+if __name__ == "__main__":
+    main()
