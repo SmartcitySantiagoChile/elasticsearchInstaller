627f4ca26ca3b3c7488b2df4ba99ac68c0ccafc1
Felipe Hernández <cephei.1313@gmail.com>
2018-03-06 09:34:39 -0300

    refactoring

--- replace_config_psql.py
+++ replace_config_psql.py
@@ -5,31 +5,35 @@ import sys
 import os
 
 
+def update_pg_hba_file(postgresql_version):
+    path = '/etc/postgresql/{0}/main/pg_hba.conf'.format(postgresql_version)
+    additional_line = 'local   all             all                                     md5\n'
+
+    new_lines = []
+    header_found = False
+
+    with open(path) as FILE:
+        for line in FILE:
+            if header_found and line != additional_line:
+                # if additional line was added previously, ignore it
+                new_lines.append(additional_line)
+                os.system("echo done")
+
+            if line == '''# "local" is for Unix domain socket connections only\n''':
+                header_found = True
+
+            new_lines.append(line)
+
+    with open(path, 'w') as CONFIG_FILE:
+        for line in new_lines:
+            CONFIG_FILE.write(line)
+
+
 def main():
     if len(sys.argv) < 2:
         pass
     else:
-        path = '/etc/postgresql/{0}/main/pg_hba.conf'.format(sys.argv[1])
-        additional_line = 'local   all             all                                     md5\n'
-
-        new_lines = []
-        header_found = False
-
-        with open(path) as FILE:
-            for line in FILE:
-                if header_found and line != additional_line:
-                    # if additional line was added previously, ignore it
-                    new_lines.append(additional_line)
-                    os.system("echo done")
-
-                if line == '''# "local" is for Unix domain socket connections only\n''':
-                    header_found = True
-
-                new_lines.append(line)
-
-        with open(path, 'w') as CONFIG_FILE:
-            for line in new_lines:
-                CONFIG_FILE.write(line)
+        update_pg_hba_file(sys.argv[1])
 
 
 if __name__ == "__main__":
