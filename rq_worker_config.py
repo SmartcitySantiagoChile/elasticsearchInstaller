fb0f556d91c5ebd6a11a27dc3675b7529f3ddf8b
Felipe Hernández <cephei.1313@gmail.com>
2018-03-15 12:36:54 -0300

    added --queue-class param

--- rq_worker_config.py
+++ rq_worker_config.py
@@ -31,7 +31,7 @@ trap 'kill -TERM $(jobs -p); trap - INT;' TERM
 while IFS='|' read -r -a line || [[ -n "$line" ]]; do
   for (( i=0; i<"${{line[0]}}"; i++ )) do
     {1} {0}/manage.py \
-    rqworker ${{line[1]}} --worker-class "rqworkers.${{line[2]}}" &
+    rqworker ${{line[1]}} --worker-class "rqworkers.${{line[2]}}" --queue-class rqworkers.killClass.KillQueue &
     done
 done < {0}/rqworkers/worker_config.txt
 # First wait keeps the process running, waiting the workers to end
